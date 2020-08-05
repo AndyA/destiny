9f39d088682ae8b07af1a3806adf87d27b1c3403
Andy Armstrong <andy@hexten.net>
2020-08-05 13:28:51 +0100

    Link dupes

--- bin/destiny.js
+++ bin/destiny.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 "use strict";
 
-const { Destiny } = require("../lib");
+const _ = require("lodash");
 const yargs = require("yargs");
+const fs = require("fs");
+const { forceLink } = require("../lib/fsx");
+const { Destiny } = require("../lib");
 
 const argv = yargs
   .command(
@@ -16,10 +19,42 @@ const argv = yargs
       });
     },
     async argv => {
+      if (argv.n) throw new Error(`Can't dry run update`);
       for (const dir of argv.dirs) {
         const d = new Destiny({ dir });
         await d.update();
       }
     }
   )
-  .demandCommand().argv;
+  .command(
+    "link [dirs...]",
+    "Link duplicate files",
+    yargs => {
+      yargs.positional("dirs", {
+        describe: "Directories to visit",
+        type: "string",
+        default: "."
+      });
+    },
+    async argv => {
+      for (const dir of argv.dirs) {
+        const d = new Destiny({ dir });
+        const current = await d.current;
+
+        for (const set of current.duplicates) {
+          d.log(`Linking ${set[0][0].hash}`);
+          const [[target], ...others] = set;
+          for (const obj of _.flatten(others)) {
+            d.log(`  ${target.name} -> ${obj.name}`);
+            if (!argv.n) await forceLink(target.name, obj.name);
+          }
+        }
+      }
+    }
+  )
+  .alias("n", "dry-run")
+  .describe("n", "Don't change anything")
+  .boolean("n")
+  .demandCommand()
+  .help("h")
+  .alias("h", "help").argv;
